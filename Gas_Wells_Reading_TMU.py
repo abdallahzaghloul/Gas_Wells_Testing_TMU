391464361d6cc3a71a7da08203ece94b8e582d7c
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-10 05:34:32 +0300

    Update Gas_Wells_Reading_TMU.py

--- Gas_Wells_Reading_TMU.py
+++ Gas_Wells_Reading_TMU.py
@@ -4,6 +4,6 @@ import streamlit as st
 import numpy as np #1
 import pandas as pd #2
 import datetime
-im = Image.open("KPC.JPG")
+im = Image.open("KPC.png")
 image = np.array(im)
 st.image("image")
