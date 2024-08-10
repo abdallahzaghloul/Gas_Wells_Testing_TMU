5872b0b1abd5ed3b18aaec396e94b6e92ae25b82
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-10 05:06:50 +0300

    Update Gas_Wells_Reading_TMU.py

--- Gas_Wells_Reading_TMU.py
+++ Gas_Wells_Reading_TMU.py
@@ -4,6 +4,6 @@ import streamlit as st
 import numpy as np #1
 import pandas as pd #2
 import datetime
-File="Client_EPIS_Daily_Progress.xlsx"
+#File="Client_EPIS_Daily_Progress.xlsx"
 im = Image.open("KPC.jpg")
 image = np.array(im)
