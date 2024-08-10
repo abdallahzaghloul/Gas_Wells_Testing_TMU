27e53a448d7ef64548f57be20b683c7f70f28406
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-10 06:01:02 +0300

    Update Gas_Wells_Reading_TMU.py

--- Gas_Wells_Reading_TMU.py
+++ Gas_Wells_Reading_TMU.py
@@ -10,4 +10,10 @@ st.image(image)
 st.markdown(" <center>  <h1> Well_Name Testing Follow UP </h1> </font> </center> </h1> ",
             unsafe_allow_html=True)
 
+Well_Name = st.selectbox(
+...     'The Well Name for current Testing',
+...     ('BARAKAT-D01X','BARAKAT-D016','BARAKAT-D016','FUSTAT-N01X','IO-01X','BAT-10X','NUT-01X','SHAI-01X','ATOUN-N01X','APRIES-E01X','APRIES-E03X','ANTI-01X'))
+>>>
+>>> st.write('You selected:', Well_Name)
+
 
