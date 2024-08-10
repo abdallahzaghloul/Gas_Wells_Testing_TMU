a1ef765bebb9f1ae531bb40420add23ebde8e8a2
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-10 06:44:51 +0300

    Update Gas_Wells_Reading_TMU.py

--- Gas_Wells_Reading_TMU.py
+++ Gas_Wells_Reading_TMU.py
@@ -11,13 +11,11 @@ st.markdown(" <center>  <h1> Well_Name Testing Follow UP </h1> </font> </center>
             unsafe_allow_html=True)
 
 Well_Name = st.selectbox('The Well Name for current Testing',('BARAKAT-D01X','BARAKAT-D02X','BARAKAT-D06X','FUSTAT-N01X','IO-01X','BAT-10X','NUT-01X','SHAI-01X','ATOUN-N01X','APRIES-E01X','APRIES-E03X','ANTI-01X'))
-st.write('You selected:', Well_Name)
 
 
 from datetime import time
 
 Reading_Registeration = st.slider("Reading Registeration:", value=(time(11, 30)))
 
-st.write("You're scheduled for:", Reading_Registeration)
 
 
