b2d359ec4bf9b177902e2b9f2aa13cbd35a488a6
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-11 22:30:58 +0300

    Update Gas_Wells_Reading_TMU.py

--- Gas_Wells_Reading_TMU.py
+++ Gas_Wells_Reading_TMU.py
@@ -4,6 +4,7 @@ import streamlit as st
 import numpy as np #1
 import pandas as pd #2
 import datetime
+from github import Github
 im = Image.open("KPC.png")
 image= np.array(im)
 st.image(image)
@@ -116,12 +117,14 @@ st.write("SAL KPPM  = ",Sal)
 
 
 
+#if st.button('Save'):
+# with pd.ExcelWriter('Gas_Wells_Test_Readings.xlsx') as writer:  
+#    df0.to_excel(writer, sheet_name='Sheet1')
+
 if st.button('Save'):
- with pd.ExcelWriter('Gas_Wells_Test_Readings.xlsx') as writer:  
-    df0.to_excel(writer, sheet_name='Sheet1')
-
-
-
+with open('vars.txt', 'r') as file:
+    for line in file:
+        vars.append(line.replace('\n', ''))
 
 
 
