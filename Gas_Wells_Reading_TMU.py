191499925ad20af33927528fda89206e58c86ac4
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-11 09:48:33 +0300

    Update Gas_Wells_Reading_TMU.py

--- Gas_Wells_Reading_TMU.py
+++ Gas_Wells_Reading_TMU.py
@@ -59,7 +59,7 @@ st.dataframe(Data)
 
 
 if st.button('Save'):
- with pd.excelwriter('Gas_Wells_Readings.xlsx', engine='openpyxl', mode='a') as writer:
+ with pd.ExcelWriter('Gas_Wells_Readings.xlsx', engine='openpyxl', mode='a') as writer:
   new_df.to_excel(writer, sheet_name='Sheet1', index=False, header=None)
 
 
