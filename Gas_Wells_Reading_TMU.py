215df841b40f47ba6b3287a59645c99f4ca74c07
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-11 22:33:47 +0300

    Update Gas_Wells_Reading_TMU.py

--- Gas_Wells_Reading_TMU.py
+++ Gas_Wells_Reading_TMU.py
@@ -109,7 +109,7 @@ df1["API"]=df1["API"].astype("str")+ " Deg"
 
 
 
-st.dataframe(df1)
+st.dataframe(df1,width=100, height = 500)
 st.write("CO2 %  = ",CO2)
 st.write("H2S PPM  = ",H2S)
 st.write("SAL KPPM  = ",Sal)
