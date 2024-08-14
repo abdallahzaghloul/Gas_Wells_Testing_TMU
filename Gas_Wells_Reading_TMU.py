9a154f809f17907ebe1b2f183a013f7b0036fc2b
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-14 20:01:31 +0300

    Update Gas_Wells_Reading_TMU.py

--- Gas_Wells_Reading_TMU.py
+++ Gas_Wells_Reading_TMU.py
@@ -12,7 +12,7 @@ from googleapiclient.http import MediaIoBaseDownload
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+from googleapiclient.errors import HttpError as error
 gauth=GoogleAuth()
 
 
