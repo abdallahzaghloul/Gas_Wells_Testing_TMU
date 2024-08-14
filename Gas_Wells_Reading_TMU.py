2b455b14374b8d407ba46c6e6ffdc48a94271058
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-14 19:01:43 +0300

    Update Gas_Wells_Reading_TMU.py

--- Gas_Wells_Reading_TMU.py
+++ Gas_Wells_Reading_TMU.py
@@ -14,10 +14,10 @@ from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 #from googleapiclient.errors import HTTpError
+gauth=GoogleAuth()
 
 drive=GoogleDrive(gauth)
 
-gauth=GoogleAuth()
 
 import os
 SCOPES=["https://googleapis.com/auth/spreadsheets"]
