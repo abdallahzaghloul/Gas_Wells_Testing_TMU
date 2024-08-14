8fba82fc6439fc6b1e7e6477a254be56221a2f7f
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-14 19:00:07 +0300

    Update Gas_Wells_Reading_TMU.py

--- Gas_Wells_Reading_TMU.py
+++ Gas_Wells_Reading_TMU.py
@@ -17,7 +17,7 @@ import os
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-#from googleapiclient.errors import HTTpError
+from googleapiclient.errors import HTTpError
 SCOPES=["https://googleapis.com/auth/spreadsheets"]
 
 gauth.LocalWebserverAuth()
