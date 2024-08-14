2e014ae895f2c2a963a7edabeb5193fcf08280c0
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-14 19:40:15 +0300

    Update Gas_Wells_Reading_TMU.py

--- Gas_Wells_Reading_TMU.py
+++ Gas_Wells_Reading_TMU.py
@@ -20,11 +20,9 @@ gauth=GoogleAuth()
 import os
 SCOPES=["https://googleapis.com/auth/spreadsheets"]
 SPREADSHEET_ID = "1Z0clIbSazOxcYwngdQGK557s-ltIQ-Al_Ja5ypl2fgw"
-client_secrets= json.load('client_secrets.json')
-gauth.LocalWebserverAuth(client_secrets)
+#gauth.LocalWebserverAuth(client_secrets)
 drive=GoogleDrive(gauth)
 
-#gauth.LocalWebserverAuth()
 
 url=("https://github.com/abdallahzaghloul/Gas_Wells_Testing_TMU/blob/main/Gas_Wells_Reading.xlsx?raw=true")
 
