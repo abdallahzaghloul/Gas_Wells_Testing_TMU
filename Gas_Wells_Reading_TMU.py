6e0a5dfff6bb22378f9c600c296b6b88d96ae9ea
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-16 06:21:18 +0300

    Update Gas_Wells_Reading_TMU.py

--- Gas_Wells_Reading_TMU.py
+++ Gas_Wells_Reading_TMU.py
@@ -90,11 +90,11 @@ if st.button('Save New Reading'):
    st.title("Your Readings have been saved successfully")          
    conn.create(worksheet=worksheet)
    conn.update(spreadsheet="https://docs.google.com/spreadsheets/d/1Z0clIbSazOxcYwngdQGK557s-ltIQ-Al_Ja5ypl2fgw",worksheet=worksheet,data=df1)
- for i in range (1,48):           
-  try:
-   st.write(conn.read(worksheet=Well_ID+"_"+str(i)) )            
-  except:
-   pass        
+  for i in range (1,48):           
+   try:
+    st.write(conn.read(worksheet=Well_ID+"_"+str(i)) )            
+   except:
+    pass        
            
  except:
    st.markdown(" <right>  <h1> Please Update Reading No. OR Reading Time </h1> </font> </right> </h1> ",unsafe_allow_html=True)
