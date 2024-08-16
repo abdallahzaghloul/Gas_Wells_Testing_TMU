e19f7ee96fb43ab8b35e5808d4c66bfd87e4aaf2
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-16 14:09:23 +0300

    Update Gas_Wells_Reading_TMU.py

--- Gas_Wells_Reading_TMU.py
+++ Gas_Wells_Reading_TMU.py
@@ -9,7 +9,7 @@ import datetime
 im = Image.open("KPC.png")
 image= np.array(im)
 st.image(image)
-st.markdown(" <center>  <h1> Well_Name Testing Follow UP </h1> </font> </center> </h1> ",
+st.markdown(" <center>  <h1> Gas Wells Testing Follow UP </h1> </font> </center> </h1> ",
             unsafe_allow_html=True)
 
 Well_Name = st.selectbox('The Well Name for current Testing',('BARAKAT-D01X','BARAKAT-D02X','BARAKAT-D06X','FUSTAT-N01X','IO-01X','BAT-10X','NUT-01X','SHAI-01X','ATOUN-N01X','APRIES-E01X','APRIES-E03X','ANTI-01X'))
