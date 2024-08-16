062481733484b9b8b2dbdeed37e67e3cfe0ff463
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-16 03:36:17 +0300

    Update Gas_Wells_Reading_TMU.py

--- Gas_Wells_Reading_TMU.py
+++ Gas_Wells_Reading_TMU.py
@@ -85,8 +85,8 @@ if st.button('Save'):
  
  st.title("Read Google Sheet as DataFrame")
  conn = st.experimental_connection("gsheets", type=GSheetsConnection)
- existing_data = conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1Z0clIbSazOxcYwngdQGK557s-ltIQ-Al_Ja5ypl2fgw",ttl=10)
- st.write(existing_data)
+ #existing_data = conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1Z0clIbSazOxcYwngdQGK557s-ltIQ-Al_Ja5ypl2fgw",ttl=10)
+ #st.write(existing_data)
  conn.update(spreadsheet="https://docs.google.com/spreadsheets/d/1Z0clIbSazOxcYwngdQGK557s-ltIQ-Al_Ja5ypl2fgw",data=df1)
 
 
