77d0a56f61f1185f7353b20ee77d36d03bc7aeb0
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-17 05:47:48 +0300

    Update Gas_Wells_Reading_TMU.py

--- Gas_Wells_Reading_TMU.py
+++ Gas_Wells_Reading_TMU.py
@@ -121,6 +121,6 @@ with col1:
 #  conn.update(spreadsheet="https://docs.google.com/spreadsheets/d/1Z0clIbSazOxcYwngdQGK557s-ltIQ-Al_Ja5ypl2fgw",worksheet=sparesheet,data=df1)
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 Last_Reading = conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1Z0clIbSazOxcYwngdQGK557s-ltIQ-Al_Ja5ypl2fgw",worksheet="Sheet1")
-n=Last_Reading['Reading_No'].loc[0]
+n=Last_Reading['Reading_No'].loc[1]
 st.write(n)
 st.write(Last_Reading)
